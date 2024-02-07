add911edda3b2ecce93dc50d54c1a2087e23a16c
Waldirio M Pinheiro <waldirio@gmail.com>
2024-02-06 19:22:11 -0800

    - Adding the ability to deploy anywhere, once /var will require admin privileges. - Also checking that there is a missing script, settings.server.rb to rename the cert entry

--- intermediate.ca.rb
+++ intermediate.ca.rb
@@ -2,7 +2,8 @@
 
 require 'fileutils'
 
-file = File.open("/var/ca-scripts/settings/intermediate.ca.conf")
+# file = File.open("/var/ca-scripts/settings/intermediate.ca.conf")
+file = File.open("settings/intermediate.ca.conf")
 file_data = file.readlines.map(&:chomp)
 file_data.reject! { |c| c.empty? }
 file.close
@@ -18,7 +19,8 @@ settings.each do |key, value|
   settings[key].strip!
 end
 
-opensslcnf = `cat /var/ca-scripts/openssl/intermediate.openssl.cnf`
+# opensslcnf = `cat /var/ca-scripts/openssl/intermediate.openssl.cnf`
+opensslcnf = `cat openssl/intermediate.openssl.cnf`
 
 settings.each do |key, value|
   opensslcnf.gsub!("#{key.to_s.upcase}","#{value}")
@@ -27,7 +29,8 @@ end
 FileUtils.mkdir_p("#{settings[:directory]}/#{settings[:int_name]}")
 File.write("#{settings[:directory]}/#{settings[:int_name]}/#{settings[:int_name]}.openssl.cnf", opensslcnf)
 
-script = `cat /var/ca-scripts/scripts/intermediate-ca.sh`
+# script = `cat /var/ca-scripts/scripts/intermediate-ca.sh`
+script = `cat scripts/intermediate-ca.sh`
 settings.each do |key, value|
   script.gsub!("#{key.to_s.upcase}","#{value}")
 end
