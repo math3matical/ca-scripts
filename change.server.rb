ef7167a192eb73e096087cced1b019613f5843bf
Waldirio M Pinheiro <waldirio@gmail.com>
2024-02-23 17:34:10 -0800

    updated readme and change.server.rb, removing the /var/... path

--- change.server.rb
+++ change.server.rb
@@ -20,10 +20,12 @@ end
 if answer.match?('n') || !answer.match?('y') 
   exit
 else
-  file_name = '/var/ca-scripts/settings/server.cert.conf'
+#  file_name = '/var/ca-scripts/settings/server.cert.conf'
+  file_name = 'settings/server.cert.conf'
   text = File.read(file_name)
   new_contents = text.gsub(/common_name:.*\R+/,"common_name: #{common_name}")
   new_contents = new_contents.gsub(/srv_fqdn:.*\R+/,"srv_fqdn: #{srv_fqdn}")
-  File.write('/var/ca-scripts/settings/server.cert.conf', new_contents)
+#  File.write('/var/ca-scripts/settings/server.cert.conf', new_contents)
+  File.write('settings/server.cert.conf', new_contents)
 end
 
